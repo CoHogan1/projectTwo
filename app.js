8a83daba45fc830065a413256a8c4ea1652ac7a2
Conner Hogan <connerrhogan@gmail.com>
2021-03-16 19:26:35 -0600

    App is listening

---
+++ app.js
@@ -0,0 +1,27 @@
+// whatever I want to call my app
+const express = require('express')
+const PORT = 3000
+const app = express()
+
+app.use(express.json())
+//app.use(express.url({extended: true}))
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+
+
+
+
+
+app.get('/home', (req, res)=>{
+    console.log('Running')
+    res.send(`<h1>This is working!</h1>`)
+})
+
+
+
+
+
+app.listen(PORT, (req, res)=>{
+    console.log('app is listening')
+})

45fd2a9a54d2f89203f95b3c7c7cb7cf583c1dbe
Conner Hogan <connerrhogan@gmail.com>
2021-03-17 18:39:16 -0600

    Edit route working correctly. finally

--- app.js
+++ app.js
@@ -31,9 +31,10 @@ db.on('error', (err)=> console.log(err.message + ' Mongo is not running!!!'))
 db.on('connected', ()=> console.log('Mongo connected: ' + mongoURI))
 db.on('disconnected', ()=> console.log('Mongo is now Disconnected, Have a good day!'))
 
+app.use(express.urlencoded({extended:true}))
 
 //==============================================================================
-// send files to database
+//--------------------------send files to database
 
 // Attempt.create(information, (err, totalData)=>{
 //     if (err) {
@@ -44,10 +45,12 @@ db.on('disconnected', ()=> console.log('Mongo is now Disconnected, Have a good d
 //     db.close()
 // })
 
+//--------------------------clear database, and check database contents.
+
 // Attempt.remove({}, (err, allData)=>{ // remove all data from DB
 //     console.log(allData)
 // })
-
+//
 // Attempt.find({}, (err, everything)=>{
 //     console.log(everything)
 //     db.close()
@@ -56,15 +59,21 @@ db.on('disconnected', ()=> console.log('Mongo is now Disconnected, Have a good d
 //==============================================================================
 
 
-
-
-
 // controllers ---------------------------------------------------------------
 app.get('/home', (req, res)=>{
     console.log('Running')
+    // Attempt.create(req.body, (err, genesis)=>{
+    //     if (err) {
+    //         console.log(err)
+    //     } else {
+    //         console.log(genesis)
+    //         res.redirect('/home/')
+    //     }
+    //})
     res.send(`<h1>This is working!</h1>`)
 })
 
+//-----show---------------------------------------------------------------------
 app.get('/home/index', (req, res)=>{
     console.log('index route')
     Attempt.find({}, (err, data, next)=>{
@@ -76,6 +85,21 @@ app.get('/home/index', (req, res)=>{
         }
     })
 })
+
+app.put('/home/index', (req, res)=>{
+    console.log('pposting to home/index route')
+    Attempt.create(req.body, (err, genesis)=>{ // probably dont need this create.
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(genesis + " this is the req.body")
+            res.redirect('/home/index')
+        }
+    })
+})
+
+//-----show---------------------------------------------------------------------
+
 
 app.get('/home/index/:id', (req, res)=>{
     Attempt.findById(req.params.id, (err, system)=>{
@@ -91,10 +115,35 @@ app.get('/home/index/:id/edit', (req, res)=>{
     Attempt.findById(req.params.id, (err, program)=>{
         //console.log(program)
         //res.send("This is working")
-        res.render('edit.ejs', { practices: program})
+        res.render('edit.ejs', { practices: program })
     })
 })
 
+
+// --------------2 of 2---------------------------------------------------------
+app.put('/home/index/:id', (req, res)=>{
+    //console.log(req.body) // this is working
+    if (req.body.didItWork === 'on') {
+        req.body.didItWork = true
+    } else {
+        req.body.didItWork = false
+    }
+    console.log(req.body)
+    console.log(req.params.id)
+    //res.send("Checking to see if this works")  // working
+    Attempt.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true},
+        (err, updates)=>{
+            console.log(updates)
+            //res.send(updates)
+            res.redirect('/home/index')
+    })
+})
+
+
+//------------------------------------------------------------------------------
 
 
 
--- models/schema.js
+++ models/schema.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+
 const procedueSchema = new Schema ({
     name: String,
     didItWork: {type: Boolean, default: false},
     myRating: Number,
     results: String,
     notes: String,
-
 },{timestamps : true})
+
 
 const Attempt = mongoose.model('attempt', procedueSchema)
 
